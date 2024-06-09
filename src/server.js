80c0603148f55f80c44aa06caa1b643d7ae56849
spaceTony9 <bilokon.fw7@gmail.com>
2024-06-09 10:54:38 +0200

    added status codes to res

--- src/server.js
+++ src/server.js
@@ -60,7 +60,7 @@ export const setupServer = () => {
     }
   });
 
-  app.use('*', (req, res, next) => {
+  app.use('*', (req, res) => {
     res.status(404).json({
       message: 'Not found',
     });
