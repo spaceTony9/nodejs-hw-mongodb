ea4650bcaec6294ce3dfcb73dcc4fb07b9f6c2ec
spaceTony9 <bilokon.fw7@gmail.com>
2024-07-09 12:18:24 +0200

    Update auth.js

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -35,7 +35,7 @@ router.post('/refresh', ctrlWrapper(refreshUserSessionController));
 router.post('/logout', ctrlWrapper(logoutUserController));
 
 router.post(
-  '/request-reset-email',
+  '/send-reset-email',
   validateBody(requestResetEmailSchema),
   ctrlWrapper(requestResetEmailController)
 );
